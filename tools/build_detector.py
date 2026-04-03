2760d6f4fa1be7d06036d5dfa717daf48f11e7a2
Thomash02 <tuomas.haapasalo02@gmail.com>
2026-04-03 14:00:21 +0000

    fix: tools checkpoint path

--- tools/build_detector.py
+++ tools/build_detector.py
@@ -23,7 +23,7 @@ class HumanDetector:
             from sam3.model_builder import build_sam3_image_model
             from sam3.model.sam3_image_processor import Sam3Processor
             
-            self.detector = build_sam3_image_model()
+            self.detector = build_sam3_image_model(checkpoint_path=kwargs["path"])
             self.processor = Sam3Processor(self.detector)
             self.detector_func = lambda detector, img, **kwargs: self.sam3_run(
                 img, **kwargs
